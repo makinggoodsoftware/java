d2baaf4e8dcd244a5b0ac84ea91edcdeefbc688c
makinggoodsoftware <alberto.gutierrez.campo@gmail.com>
2012-11-28 08:10:13 +0000

    BASELINE: Working version displaying an hexagon within a rectangle

--- invasionII/src/com/mgs/invasion/Main.java
+++ invasionII/src/com/mgs/invasion/Main.java
@@ -1,13 +1,12 @@
 package com.mgs.invasion;
 
-import com.mgs.fantasi.ui.wireframe.Wireframe;
 import com.mgs.fantasi.polygon.HexagonShape;
 import com.mgs.fantasi.structures.PolygonStructureBuilder;
 import com.mgs.fantasi.structures.RectangleStructureBuilder;
-import com.mgs.fantasi.structures.VerticalSlicesStructureBuilder;
 import com.mgs.fantasi.ui.driver.UIDriver;
 import com.mgs.fantasi.ui.profile.ExpandToParent;
 import com.mgs.fantasi.ui.profile.UIProfileFactory;
+import com.mgs.fantasi.ui.wireframe.Wireframe;
 import com.mgs.invasion.mvc.view.DebugUIProfileFactory;
 
 import javax.swing.*;
@@ -22,14 +21,14 @@ public class Main {
 		UIDriver<JPanel> uiDriver = UIDriver.forSwing(uiProfileFactory.getUIProfile());
 
 		Wireframe content =
-			new VerticalSlicesStructureBuilder(
+//			new VerticalSlicesStructureBuilder(
 				new RectangleStructureBuilder().
 				witchContent
 				(
 					new PolygonStructureBuilder(new HexagonShape()).
 					withSizeStrategy(new ExpandToParent())
-				)
-			).
+				).
+//			).
 			build();
 
 		uiDriver.show(content, new Dimension(400,400));

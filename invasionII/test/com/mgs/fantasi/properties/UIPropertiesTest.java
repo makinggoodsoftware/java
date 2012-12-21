e70963f2a71e323479768f0ae35f38945ea07c0f
makinggoodsoftware <alberto.gutierrez.campo@gmail.com>
2012-12-21 09:44:39 +0000

    REFACTOR Adding more tests

--- invasionII/test/com/mgs/fantasi/properties/UIPropertiesTest.java
+++ invasionII/test/com/mgs/fantasi/properties/UIPropertiesTest.java
@@ -1,12 +1,16 @@
 package com.mgs.fantasi.properties;
 
+import com.mgs.fantasi.profile.UIStyle;
 import com.mgs.fantasi.views.BaseView;
 import org.junit.Test;
 
 import static com.mgs.fantasi.properties.BorderDefinition.noBorder;
+import static com.mgs.fantasi.properties.ColorDefinition.colorFromAwtColor;
 import static com.mgs.fantasi.properties.ColorDefinition.noColor;
 import static com.mgs.fantasi.properties.measurements.EmptyMeasurement.emptyMeasurement;
 import static com.mgs.fantasi.rendering.Padding.noPadding;
+import static java.awt.Color.RED;
+import static java.awt.Color.YELLOW;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 
@@ -22,5 +26,28 @@ public class UIPropertiesTest {
 		assertEquals(uiProperties.getPadding(), noPadding());
 		assertTrue(uiProperties.getShape() instanceof BaseView.NativeRectanguarShape);
 	}
+
+	@Test
+	public void testApplyStyle_whenPropertiesAndStyleAreCompletelyEmpty(){
+		UIProperties uiProperties = new UIProperties();
+		uiProperties.applyStyle(new UIStyle());
+
+		assertEquals(uiProperties.getBackgroundColor(), noColor());
+		assertEquals(uiProperties.getBorder(), noBorder());
+	}
+
+	@Test
+	public void testApplyStyle_whenPropertiesAreEmptyAndStyleIsFullyDefined(){
+		UIProperties uiProperties = new UIProperties();
+		UIStyle uiStyle = new UIStyle().
+			withBackground(colorFromAwtColor(YELLOW)).
+			withBorder(new BorderDefinition(colorFromAwtColor(RED), 3));
+		uiProperties.applyStyle(uiStyle);
+
+		assertEquals(uiProperties.getBackgroundColor(), colorFromAwtColor(YELLOW));
+		assertEquals(uiProperties.getBorder().getColor(), colorFromAwtColor(RED));
+		assertEquals(uiProperties.getBorder().getWidth(), 3);
+	}
+
 
 }

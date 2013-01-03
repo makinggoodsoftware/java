f5d8972072fe5f5018c8b2360c2fbea57785e5fb
makinggoodsoftware <alberto.gutierrez.campo@gmail.com>
2013-01-03 18:52:23 +0000

    REFACTORING Adding generics into RenderingContextFactory and RenderingContext, getting rid of UINativeRenderer

--- invasionII/src/main/java/com/mgs/fantasi/driver/swing/RenderingContextFactory.java
+++ invasionII/src/main/java/com/mgs/fantasi/driver/swing/RenderingContextFactory.java
@@ -1,22 +1,7 @@
 package com.mgs.fantasi.driver.swing;
 
-import com.mgs.fantasi.driver.swing.jPanelCreation.JPanelCreationStrategyFactory;
-import com.mgs.fantasi.driver.swing.layoutConstruction.LayoutConstructionManager;
-import com.mgs.fantasi.styles.StyleManager;
 import com.mgs.fantasi.styles.UIProfile;
 
-public class RenderingContextFactory {
-	private final LayoutConstructionManager layoutConstructionManager;
-	private final StyleManager styleManager;
-	private final JPanelCreationStrategyFactory jPanelCreationStrategyFactory;
-
-	public RenderingContextFactory(LayoutConstructionManager layoutConstructionManager, StyleManager styleManager, JPanelCreationStrategyFactory jPanelCreationStrategyFactory) {
-		this.layoutConstructionManager = layoutConstructionManager;
-		this.styleManager = styleManager;
-		this.jPanelCreationStrategyFactory = jPanelCreationStrategyFactory;
-	}
-
-	public RenderingContext getContextFor(UIProfile uiProfile) {
-		return new RenderingContext(uiProfile, layoutConstructionManager, styleManager, jPanelCreationStrategyFactory);
-	}
+public interface RenderingContextFactory<T> {
+	RenderingContext<T> getContextFor(UIProfile uiProfile);
 }

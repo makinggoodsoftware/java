36b58078ddf81aa115c391bdbd3327b9057e7c44
makinggoodsoftware <alberto.gutierrez.campo@gmail.com>
2013-01-03 18:52:39 +0000

    REFACTORING Adding generics into RenderingContextFactory and RenderingContext, getting rid of UINativeRenderer

--- invasionII/src/main/java/com/mgs/fantasi/driver/UINativeRenderer.java
+++
@@ -1,8 +0,0 @@
-package com.mgs.fantasi.driver;
-
-import com.mgs.fantasi.styles.UIProfile;
-import com.mgs.fantasi.views.View;
-
-public interface UINativeRenderer<T> {
-	T render(View renderable, UIProfile uiProfile);
-}
